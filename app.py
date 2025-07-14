01742516e26491ca19af0ddcc69c80c595e022ae
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-14 11:50:55 +0330

    Removed main clause

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from application.config import logging_config, flask_run_configs
 from application.utils import init_logging
 from application import create_app
 
-def main() -> int:
-    init_logging(**logging_config)
-    app = create_app(__name__)
-    app.run(**flask_run_configs)
-    return 0
-
-if __name__ == '__main__':
-    sys.exit(main())
+init_logging(**logging_config)
+app = create_app(__name__)
+app.run(**flask_run_configs)
