d0f75f78f2b44110dad002cf1abaddddda38077a
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-16 11:38:04 +0330

    Improvements

--- application/api_connections/_api_tester.py
+++ application/api_connections/_api_tester.py
@@ -6,16 +6,16 @@ from classmods import suppress_errors
 class APITester:
     @staticmethod
     @suppress_errors('exception')
-    def test_ami(ami: AMIClient) -> str | Exception:
+    def test_ami(ami: AMIClient) -> str:
         raise NotImplementedError('No implementation provided from AMIClient')
     
     @staticmethod
     @suppress_errors('exception')
-    def test_netbox(netbox: NetboxClient) -> str | Exception:
+    def test_netbox(netbox: NetboxClient) -> str:
         return netbox.version
 
     @staticmethod
     @suppress_errors('exception')
-    def test_sarv(sarv: SarvClient) -> str | Exception:
+    def test_sarv(sarv: SarvClient) -> str:
         sarv.login()
         return "v'5.0.0'" if sarv.url.endswith("API.php") else sarv.url.split('/')[5].replace('_', '.')
--- application/utils/_decorators.py
+++ application/utils/_decorators.py
@@ -7,5 +7,5 @@ from ..api_connections import netbox_connection
 
 def token_required() -> Any:
     if 'Authorization' in request.headers:
-        _, token = request.headers.get('Authorization', ' ').split('Bearer ', 1)
+        _, token = request.headers.get('Authorization', ' ').split('Token ', 1)
         tenant = netbox_connection.tenancy
