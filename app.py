ec657a29338313aa4108b752b12a20db592181fa
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-16 12:28:26 +0330

    Improvements

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import sys
+from application import create_app
 from application.config import logging_config, flask_run_configs
 from application.utils import init_logging
-from application import create_app
 
 init_logging(**logging_config)
 app = create_app(__name__)
