d80f94738ea7c0c8ee26f724403064abc973051a
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-20 18:40:45 +0330

    Fixed Ami Configs

--- application/api_connections/_asterisk.py
+++ application/api_connections/_asterisk.py
@@ -2,7 +2,4 @@ from ami_client import AMIClient
 
 from ..config import ami_configs
 
-ami_connection = AMIClient(
-    host = ami_configs['host'],
-    port = ami_configs['port'],
-)
+ami_connection = AMIClient(**ami_configs)
