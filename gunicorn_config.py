c6933100e370b55fb79434c37d3acc6391b33f37
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-08-12 08:43:22 +0330

    Fix gunicorn problem

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,14 @@
 bind = "0.0.0.0:8000"
 workers = 4
 timeout = 30
+
+
+def post_fork(server, worker):
+    from application.jobs import JobRegistry
+    JobRegistry.start_all()
+
+
+def worker_exit(server, worker):
+    from application.jobs import JobRegistry
+    print(f"Stopping jobs in worker PID {worker.pid}")
+    JobRegistry.stop_all()
