e8f96ad833e1a82aaebfdcfc16fe83e0856dbd67
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-08-12 08:31:07 +0330

    Fixed manage_env

--- manage_env.py
+++ manage_env.py
@@ -1,9 +1,10 @@
-import application
 from classmods import ENVMod
+from sarvcrm_api import SarvClient as _
+from ami_client import AMIClient as _
+import application.config as _
 
 
 if __name__ == '__main__':
-    app = application.create_app(__name__)  # create for ENV load
     ENVMod.save_example()
     ENVMod.sync_env_file()
     ENVMod.load_dotenv()
